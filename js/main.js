6e1fb3244408cc9181324b0ae95d9d8aeb90f156
Loki <njacobsen@thetus.com>
2013-01-26 19:54:18 -0500

    merge and up

--- js/main.js
+++ js/main.js
@@ -5,10 +5,11 @@ jQuery(document).ready(function() {
 		mode:'both', 
 		callback: function() {
 			jQuery("[data-text]").each(function() {
-				if(jQuery(this).attr('class') && ~jQuery(this).attr('class').toString().indexOf("ui-title")) {
-					$(this).text(jQuery.i18n.prop(jQuery(this).attr('data-text')));					
+				if (jQuery(this).attr('data-role') && jQuery(this).attr('data-role') == "button") {
+					$(".ui-btn-text", this).text(jQuery.i18n.prop(jQuery(this).attr('data-text')));
+				} else {
+					$(this).text(jQuery.i18n.prop(jQuery(this).attr('data-text')));	
 				}
-				$(".ui-btn-text", this).text(jQuery.i18n.prop(jQuery(this).attr('data-text')));
 			});
 		}
 	});
