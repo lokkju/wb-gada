5a1b372101090a193e59e93450bb7c64081a0028
Loki <njacobsen@thetus.com>
2013-01-26 20:33:03 -0500

    fixing js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-$(document).on('pageinit', function() {
+jQuery(document).on('pageinit',function() {
 	jQuery.i18n.properties({
 		name: 'messages',
 		path:'i18n/', 
 		mode:'both', 
 		callback: function() {
 			jQuery("[data-text]").each(function() {
-				if (jQuery(this).attr('data-role') && jQuery(this).attr('data-role') == "button") {
+				if(jQuery(this).attr('class') && ~jQuery(this).attr('class').toString().indexOf("ui-collapsible-heading")) {
 					$(".ui-btn-text", this).text(jQuery.i18n.prop(jQuery(this).attr('data-text')));
-				} else {
+				} else if(jQuery(this).attr('class') && ~jQuery(this).attr('class').toString().indexOf("ui-title")) {
+					$(this).text(jQuery.i18n.prop(jQuery(this).attr('data-text')));		
+				} else if (jQuery(this).attr('data-role') && jQuery(this).attr('data-role') == "button") {
+					$(".ui-btn-text", this).text(jQuery.i18n.prop(jQuery(this).attr('data-text')));
+				} else if (!jQuery(this).attr('data-role')){
 					$(this).text(jQuery.i18n.prop(jQuery(this).attr('data-text')));	
 				}
 			});
